2ff4410e54ac90fda5faad914f1fbc9f1e7ae905
Natalie Castro <134972160+NatalieRMCastro@users.noreply.github.com>
2024-12-16 16:15:50 -0700

    Update main.py

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -1,1 +1,16 @@
 ## A placeholder for the function that will speak to BlueSky and then store the tweets
+
+## Importing the Libraries
+import pandas as pd
+from time import gmtime, strftime
+import json
+
+
+
+## Defining the Main function
+def main():
+  continue
+
+## Checking that Main Exists
+if __name__=="__main__":
+    main()
